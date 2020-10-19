013fc9868b3d56d0f5e67b85b23a5088b50068d2
dede-amdp <73134218+dede-amdp@users.noreply.github.com>
2020-10-19 22:24:01 +0200

    Nuova modifica

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Home(),
-));
+      home: Home(),
+    ));
 
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Ciao")),
+      body: Column(
+        children: <Widget>[
+          Text("FUNZIONA!"),
+        ],
+      ),
     );
   }
 }
